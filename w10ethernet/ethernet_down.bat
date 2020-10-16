6146e53f3cdcc7a4710330e5ab08855894adc9af
Xoán <xoancastrogarcia@gmail.com>
2020-10-16 19:16:19 +0200

    Add files via upload

---
+++ w10ethernet/ethernet_down.bat
@@ -0,0 +1,13 @@
+:: Script para deshabilitar la conexión Ethernet al cerrar sesión en Windows 10
+@ECHO OFF
+
+:: Ejecutamos script en UTF-8 para mostrar las tildes
+::chcp 65001
+
+::netsh interface show interface
+
+::ECHO Deshabilitando conexión "Ethernet 2"...
+netsh interface set interface "Ethernet 2" admin=disable
+::ECHO Conexión "Ethernet 2" deshabilitada.
+
+::PAUSE
