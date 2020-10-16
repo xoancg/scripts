11083a20c2a99f00530731396eca5063c31a70d4
Xoán <xoancastrogarcia@gmail.com>
2020-10-16 19:11:18 +0200

    Add files via upload

--- ethernet_down.bat
+++ ethernet_down.bat
@@ -2,11 +2,12 @@
 @ECHO OFF
 
 :: Ejecutamos script en UTF-8 para mostrar las tildes
-chcp 65001
+::chcp 65001
 
 ::netsh interface show interface
 
-ECHO Deshabilitando conexión "Ethernet 2"...
+::ECHO Deshabilitando conexión "Ethernet 2"...
 netsh interface set interface "Ethernet 2" admin=disable
-ECHO Conexión "Ethernet 2" deshabilitada.
-PAUSE
+::ECHO Conexión "Ethernet 2" deshabilitada.
+
+::PAUSE
--- ethernet_up.bat
+++ ethernet_up.bat
@@ -2,13 +2,13 @@
 @ECHO OFF
 
 :: Ejecutamos script en UTF-8 para mostrar las tildes
-chcp 65001
+::chcp 65001
 
 ::netsh interface show interface
 
-ECHO Deshabilitando conexión "Ethernet 2"...
+::ECHO Deshabilitando conexión "Ethernet 2"...
 netsh interface set interface "Ethernet 2" admin=enable
-ECHO Conexión "Ethernet 2" deshabilitada.
-PAUSE
+::ECHO Conexión "Ethernet 2" deshabilitada.
+::PAUSE
 
 
